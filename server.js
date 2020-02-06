709648c01a8e9539035b806de3d12fc21887a9b4
Aleen <aleen42@vip.qq.com>
2020-02-06 09:45:29 +0800

    break(github): access_token query has been deprecated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     /** avoid CORS */
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
 
